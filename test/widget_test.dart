f34fa58e1e509d328963e02eaca2dd9c53f15895
Abdullah Esmail <aesmail@hey.com>
2025-03-19 21:06:52 +0300

    add more tests.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kite/features/cluster/ui/did_you_know.dart';
+import 'package:kite/features/cluster/ui/location.dart';
 import 'package:kite/features/cluster/ui/news_paragraph.dart';
+import 'package:kite/features/cluster/ui/quote.dart' show Quote;
 import 'package:kite/features/cluster/ui/timeline.dart';
 
 void main() {
@@ -70,9 +72,42 @@ void main() {
 
     expect(find.text("1"), findsOneWidget);
     expect(find.text("2"), findsOneWidget);
+    expect(find.text("3"), findsNothing);
     expect(find.text("March 2025"), findsOneWidget);
     expect(find.text("April 2025"), findsOneWidget);
+    expect(find.text("May 2025"), findsNothing);
     expect(find.text("This is what happened in March."), findsOneWidget);
     expect(find.text("April came exactly as expected."), findsOneWidget);
   });
+
+  testWidgets('test the Quote widget displaying the given content', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Quote(
+          author: "Abdullah Esmail",
+          quote: "Kite is the best",
+          sourceDomain: "kite.kagi.com",
+          sourceUrl: "https://kite.kagi.com",
+        ),
+      ),
+    );
+
+    expect(find.text("Abdullah Esmail (via kite.kagi.com)"), findsOneWidget);
+    expect(find.text("Kite is the best"), findsOneWidget);
+    expect(find.text("https://kite.kagi.com"), findsNothing);
+  });
+
+  testWidgets(
+    'test the Location widget displaying the icon with the location text',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(home: Location(location: "Fullerton, CA")),
+      );
+
+      expect(find.byIcon(Icons.pin_drop_outlined), findsOneWidget);
+      expect(find.text("Fullerton, CA"), findsOneWidget);
+    },
+  );
 }
